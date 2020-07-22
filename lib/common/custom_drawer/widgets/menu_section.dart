60a853412c19ff3b1bef637c2adc9b732d2fc341
washington <washingtoncardosodev@gmail.com>
2020-07-22 10:21:10 -0300

    Trocando de tela pelo drawer

--- lib/common/custom_drawer/widgets/menu_section.dart
+++ lib/common/custom_drawer/widgets/menu_section.dart
@@ -1,19 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 import 'package:xlo/common/custom_drawer/widgets/menu_tile.dart';
 
 class MenuSection extends StatelessWidget {
-  const MenuSection({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        MenuTile(label: "Anúncios", iconData: Icons.list, onTap: (){}, highlighted: true,),
-        MenuTile(label: "Inserir Anúncios", iconData: Icons.edit, onTap: (){}, highlighted: false),
-        MenuTile(label: "Chat", iconData: Icons.chat, onTap: (){}, highlighted: false),
-        MenuTile(label: "Favoritos", iconData: Icons.favorite, onTap: (){}, highlighted: false),
-        MenuTile(label: "Minha Conta", iconData: Icons.person, onTap: (){}, highlighted: false),
-      ]
+
+    final DrawerBloc _drawerBloc = Provider.of<DrawerBloc>(context);
+
+    void _setPage(int page){
+      Navigator.of(context).pop();
+      _drawerBloc.setPage(page);      
+    }
+
+    return StreamBuilder<int>(
+      stream: _drawerBloc.outPage,
+      builder: (context, snapshot) {
+        return Column(
+          children: <Widget>[
+            MenuTile(
+              label: "Anúncios", 
+              iconData: Icons.list, 
+              onTap: (){
+                _setPage(0);
+              }, 
+              highlighted: snapshot.data == 0,
+            ),
+
+            MenuTile(
+              label: "Inserir Anúncios", 
+              iconData: Icons.edit, 
+              onTap: (){
+                _setPage(1);
+              }, 
+              highlighted: snapshot.data == 1,
+            ),
+
+            MenuTile(
+              label: "Chat", 
+              iconData: Icons.chat, 
+              onTap: (){
+                _setPage(2);
+              }, 
+              highlighted: snapshot.data == 2,
+            ),
+
+            MenuTile(
+              label: "Favoritos", 
+              iconData: Icons.favorite, 
+              onTap: (){
+                _setPage(3);
+              }, 
+              highlighted: snapshot.data == 3,
+            ),
+
+            MenuTile(
+              label: "Minha Conta", 
+              iconData: Icons.person, 
+              onTap: (){
+                _setPage(4);
+              }, 
+              highlighted: snapshot.data == 4,
+            ),
+          ]
+        );
+      }
     );
   }
 }
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 import 'package:xlo/screens/home/home_screen.dart';
 
 class BaseScreen extends StatefulWidget {
-  BaseScreen({Key key}) : super(key: key);
 
   @override
   _BaseScreenState createState() => _BaseScreenState();
@@ -11,6 +14,30 @@ class BaseScreen extends StatefulWidget {
 class _BaseScreenState extends State<BaseScreen> {
 
   final PageController _pageController = PageController();
+
+  DrawerBloc _drawerBloc;
+  StreamSubscription _drawerSubscription;
+
+  @override
+  void didChangeDependencies() {  
+
+    final DrawerBloc drawerBloc = Provider.of<DrawerBloc>(context);
+    if(drawerBloc != _drawerBloc){
+      _drawerBloc = drawerBloc;
+
+      _drawerSubscription?.cancel();
+      _drawerSubscription = _drawerBloc.outPage.listen((page) {
+        _pageController.jumpToPage(page);
+      });
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _drawerSubscription.cancel();
+    super.dispose();  
+  }
 
   @override
   Widget build(BuildContext context) {
